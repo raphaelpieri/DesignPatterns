1d02e4ea59af954ea9f70bba7f650c1ca2f7245e
FULGENCIO Raphael (external) <raphael.fulgencio-ext@faurecia.com>
2018-08-16 17:48:46 -0300

    Created Factory and Started Abstract

---
+++ AbstractMethod.Domain/AbstractFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace AbstractMethod.Domain
+{
+    internal abstract class ContinenteFactory 
+    {
+        public abstract Herbivoro ObterHerbivoro();
+        public abstract Carnivoro ObterCarnivoro();
+    }
+}
+
+
+//1:51
---
+++ FactoryMethod.Domain/Factory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FactoryMethod.Domain
+{
+    public interface IVeiculo
+    {
+        string Limite(int limite);
+    }
+
+    public class Moto : IVeiculo
+    {
+        public string Limite(int limite) => $"Limite do veiculo MOTO: {limite}";
+    }
+
+    public class Carro : IVeiculo
+    {
+        public string Limite(int limite) => $"Limite do veiculo CARRO: {limite}";
+    }
+
+    public class Caminhoneta : IVeiculo
+    {
+        public string Limite(int limite) => $"Limite do veiculo CAMINHONETA: {limite}";
+    }
+
+    public abstract class VeiculoFactory
+    {
+        public abstract IVeiculo ObterVeiculo(string categoria);
+    }
+
+    public class ConcreteVeiculoFactory : VeiculoFactory
+    {
+        public override IVeiculo ObterVeiculo(string categoria)
+        {
+            switch (categoria)
+            {
+                case "Pesado":
+                    return new Caminhoneta();
+                case "Medio":
+                    return new Carro();
+                case "Leve":
+                    return new Moto();
+                default:
+                    throw new ApplicationException($"Veiculo {categoria} nã pode ser criado");
+            }
+        }
+    }
+
+}
